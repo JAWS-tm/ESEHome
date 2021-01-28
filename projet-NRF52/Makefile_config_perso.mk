aafa3344359a6284470a28c3c0ba3d90be91704d
POIRAUD Samuel <samuel.poiraud@eseo.fr>
2021-01-28 15:36:45 +0100

    uart ok. + ajout d'un Makefile_config_perso.mk

---
+++ projet-NRF52/Makefile_config_perso.mk
@@ -0,0 +1,11 @@
+# Source files common to all targets
+PROJECT_NAME     := NRF52
+TARGETS          := nrf52832_xxaa
+OUTPUT_DIRECTORY := _build
+
+
+# This file should not be commit if you need to change it!
+
+
+SDK_ROOT := C:\Ac6\nRF5_SDK_16.0.0_98a08e2
+PROJ_DIR := .
