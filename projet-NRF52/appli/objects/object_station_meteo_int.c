264d5cf0fbaf5334c7b4ea15edd463a14704553d
rivierth <thibault.riviere@reseau.eseo.fr>
2021-02-02 15:32:19 +0100

    test

--- projet-NRF52/appli/objects/object_station_meteo_int.c
+++ projet-NRF52/appli/objects/object_station_meteo_int.c
@@ -1,21 +1,14 @@
 /*
  * object_station_meteo_int.c
  *
- *  Created on: 02 fevrier. 2021
+ *  Created on: 02 févr. 2021
  *      Author: Thibault.R
  */
 #include "../config.h"
+#include "object_station_meteo_int.h"
 
 
 #if OBJECT_ID == OBJECT_STATION_METEO_INT
-
-
-
-
-
-
-
-
 
 
 #endif
--- projet-NRF52/appli/objects/object_station_meteo_int.h
+++ projet-NRF52/appli/objects/object_station_meteo_int.h
@@ -1,12 +1,12 @@
 /*
  * object_station_meteo_int.h
  *
- *  Created on: 02 fevrier. 2021
+ *  Created on: 02 févr. 2021
  *      Author: Thibault.R
  */
 
-#ifndef APPLI_MAIN_H_
-#define APPLI_MAIN_H_
-#include "config.h"
+#ifndef OBJECT_STATION_METEO_INT_H_
+#define OBJECT_STATION_METEO_INT_H_
+#include "../config.h"
 
-#endif /* APPLI_MAIN_H_ */
+#endif /* OBJECT_STATION_METEO_INT_H_ */
