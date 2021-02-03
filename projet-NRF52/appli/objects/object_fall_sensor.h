c91a73b2544738287c9e9f298c867b25bcc8d1f9
benbrino <nora.benbrik@reseau.eseo.fr>
2021-02-03 10:22:36 +0100

    creation object_fall_sensor.h [objects]

---
+++ projet-NRF52/appli/objects/object_fall_sensor.h
@@ -0,0 +1,15 @@
+/*
+ * object_fall_sensor.h
+ *
+ *  Created on: 3 févr. 2021
+ *      Author: norab
+ */
+
+#ifndef APPLI_OBJECTS_OBJECT_FALL_SENSOR_H_
+#define APPLI_OBJECTS_OBJECT_FALL_SENSOR_H_
+
+
+
+
+
+#endif /* APPLI_OBJECTS_OBJECT_FALL_SENSOR_H_ */
