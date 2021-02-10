a2e15242629b938f650f5346e4ed8af6a9433d48
lebihata <tanguy.lebihan@reseau.eseo.fr>
2021-02-10 11:28:52 +0100

    ajout des source et header file pour l'object wine degustation

---
+++ projet-NRF52/appli/objects/object_wine_degustation.h
@@ -0,0 +1,20 @@
+/*
+ * object_wine_degustation.h
+ *
+ *  Created on: 8 févr. 2021
+ *      Author: lebihata
+ */
+
+#ifndef APPLI_OBJECTS_OBJECT_WINE_DEGUSTATION_H_
+#define APPLI_OBJECTS_OBJECT_WINE_DEGUSTATION_H_
+
+
+static void object_wine_degustation_process_main();
+//bool_e wine_status; //la bouteille est à bonne température ou non
+
+
+
+
+
+
+#endif /* APPLI_OBJECTS_OBJECT_WINE_DEGUSTATION_H_ */
