bef4adc4aae24150c94af31939ffb95b475e3b28
benbrino <nora.benbrik@reseau.eseo.fr>
2021-02-12 10:14:01 +0100

    MAJ [battery.c] : Ajout des fonctions de mesure batterie (en mV / en %)

--- projet-NRF52/appli/common/adc.c
+++ projet-NRF52/appli/common/adc.c
@@ -10,33 +10,46 @@
 #if USE_ADC
 
 //TODO généraliser pour tout les canaux...
-
+#define ADC_MAX_NB_CHANNELS	16
 
 static nrfx_saadc_config_t adc_config;
-static nrf_saadc_channel_config_t adc_channel_2;
-static nrf_saadc_channel_config_t adc_channel_3;
-static nrf_saadc_channel_config_t adc_channel_1;
+static nrf_saadc_channel_config_t adc_channels[ADC_USE_CHANNELS_NB];
+static uint8_t channel_nb = 0;
 static bool_e adc_initialized = FALSE;
-static int8_t adc_id[ADC_CHANNEL_NB];
-static uint32_t adc_converted_value[ADC_CHANNEL_NB];
-
+static int8_t adc_id[ADC_USE_CHANNELS_NB];
+static uint32_t adc_converted_value[ADC_USE_CHANNELS_NB];
+static int8_t channel_pin_to_index[ADC_MAX_NB_CHANNELS];
 
 void ADC_init(void)
 {
 	adc_config = (nrfx_saadc_config_t)NRFX_SAADC_DEFAULT_CONFIG;
-	adc_channel_1 = (nrf_saadc_channel_config_t)NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(NRF_SAADC_INPUT_AIN1);	//P0.03 -> AN1
-	adc_channel_2 = (nrf_saadc_channel_config_t)NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(NRF_SAADC_INPUT_AIN2);	//P0.04 -> AN2
-	adc_channel_3 = (nrf_saadc_channel_config_t)NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(NRF_SAADC_INPUT_AIN3);	//P0.05 -> AN3
-
-	adc_channel_1.acq_time = SAADC_CH_CONFIG_TACQ_40us;
-	adc_channel_2.acq_time = SAADC_CH_CONFIG_TACQ_40us;
-	adc_channel_3.acq_time = SAADC_CH_CONFIG_TACQ_40us;
-
 	nrfx_saadc_init(&adc_config, NULL);
-	nrfx_saadc_channel_init(0, &adc_channel_1);
-	nrfx_saadc_channel_init(1, &adc_channel_2);
-	nrfx_saadc_channel_init(2, &adc_channel_3);
+	channel_nb = 0;
 
+#if USE_ADC_CHANNEL_AIN0
+	adc_channels[channel_nb] = (nrf_saadc_channel_config_t)NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(NRF_SAADC_INPUT_AIN0);	//P0.02 -> AN0
+	adc_channels[channel_nb].acq_time = SAADC_CH_CONFIG_TACQ_40us;
+	nrfx_saadc_channel_init(channel_nb, &adc_channels[channel_nb]);
+	channel_pin_to_index[0] = channel_nb;
+	channel_nb++;
+#endif
+
+#if USE_ADC_CHANNEL_AIN1
+
+#endif
+
+#if USE_ADC_CHANNEL_AIN2
+
+#endif
+
+#if USE_ADC_CHANNEL_AIN7
+	adc_channels[channel_nb] = (nrf_saadc_channel_config_t)NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(NRF_SAADC_INPUT_AIN7);	//P0.31 -> AN7
+	adc_channels[channel_nb].acq_time = SAADC_CH_CONFIG_TACQ_40us;
+	nrfx_saadc_channel_init(channel_nb, &adc_channels[channel_nb]);
+	channel_pin_to_index[7] = channel_nb;
+	channel_nb++;
+
+#endif
 	adc_initialized = TRUE;
 }
 
@@ -44,7 +57,8 @@ void ADC_read(uint8_t channel, nrf_saadc_value_t * value)
 {
 	if(!adc_initialized)
 		ADC_init();
-	nrfx_saadc_sample_convert(channel, value);
+	uint8_t index = channel_pin_to_index[channel];
+	nrfx_saadc_sample_convert(index, value);
 }
 
 
--- projet-NRF52/appli/common/battery.c
+++ projet-NRF52/appli/common/battery.c
@@ -13,16 +13,46 @@ static nrfx_saadc_config_t adc_config;
 static nrf_saadc_channel_config_t channel_battery;
 static bool_e adc_initialized = FALSE;
 
-void MEASURE_VBAT_init(uint8_t pin)
+void MEASURE_VBAT_init(void)
 {
-	adc_config = (nrfx_saadc_config_t)NRFX_SAADC_DEFAULT_CONFIG;
-	channel_battery = (nrf_saadc_channel_config_t)NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(pin);
-	channel_battery.acq_time = SAADC_CH_CONFIG_TACQ_40us;
-	nrfx_saadc_init(&adc_config, NULL);
-	nrfx_saadc_channel_init(0, &channel_battery);
-	adc_initialized = TRUE;
+	ADC_init();
 }
 
-/*uint16_t MEASURE_VBAT_get(uint8_t pin){
-	uint8_t Vbat_mv = ADC_getValue(pin);
-}*/
+
+/**@Measure Vbat (in mV) **/
+uint16_t MEASURE_VBAT_get(void){
+	int16_t value;
+	ADC_read(CHANNEL_ADC_MEASURE_VBAT, &value);
+	int32_t mV;
+	mV = value;
+	mV *= 3300;
+	mV /= 4096;
+
+	return mV*2;	//tension batterie = 2*tension mesurée sur l'entrée ADC (pont diviseur /2 !)
+}
+
+/**@Measure Vbat (in %) **/
+uint8_t MEASURE_VBAT_get_level(void){
+	uint8_t Vbat_percent = mvToPercent(MEASURE_VBAT_get());
+	return Vbat_percent;
+}
+
+uint8_t mvToPercent(float mvolts) {
+  uint8_t battery_level;
+
+  if (mvolts >= 4200) {
+    battery_level = 100;
+  } else if (mvolts > 3900) {
+    battery_level = 100 - ((4200 - mvolts) * 58) / 100;
+  } else if (mvolts > 3800) {
+    battery_level = 42 - ((3900 - mvolts) * 24) / 160;
+  } else if (mvolts > 3600) {
+    battery_level = 18 - ((3800 - mvolts) * 12) / 300;
+  } else if (mvolts > 3000) {
+    battery_level = 6 - ((3600 - mvolts) * 6) / 340;
+  } else {
+    battery_level = 0;
+  }
+
+  return battery_level;
+}
