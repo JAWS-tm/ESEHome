94d3b68e69fdf79c860ebab91f1995135fccdf77
Montreuil <joshua.montreuil@reseau;eseo.fr>
2022-01-12 11:42:23 +0100

    Fichier test Utilisateur

--- DomESEO/src/main/java/test/UtilisateurTest.java
+++ DomESEO/src/main/java/test/UtilisateurTest.java
@@ -4,31 +4,53 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class UtilisateurTest {
+import beans.Utilisateur;
 
+class UtilisateurTest {
+	
+	private int id = 2;
+	private String pswd = "test";
+	private String pseudo = "le pseudo";
+	private int Permission = 1;
+	
+	
 	@Test
 	void testUtilisateurIntStringStringInt() {
-		fail("Not yet implemented");
+		Utilisateur util = new Utilisateur(id,pseudo,pswd,Permission);
+		
+		assertEquals(id,util.getId(),"id non correct");
+		assertTrue(util.getMdp().contains(pswd),"mdp_incorect");
+		assertTrue(util.getPseudo().contains(pseudo), "pseudo incorect");
+		assertEquals(Permission,util.getPermission(),"permission incorecte");
+		
 	}
 
 	@Test
 	void testSetId() {
-		fail("Not yet implemented");
+		Utilisateur util = new Utilisateur();
+		util.setId(id);
+		assertEquals(id,util.getId(),"id non correct");
 	}
 
 	@Test
 	void testSetMdp() {
-		fail("Not yet implemented");
+		Utilisateur util = new Utilisateur();
+		util.setMdp(pswd);
+		assertTrue(util.getMdp().contains(pswd),"mdp_incorect");
 	}
 
 	@Test
 	void testSetPermission() {
-		fail("Not yet implemented");
+		Utilisateur util = new Utilisateur();
+		util.setPermission(Permission);
+		assertEquals(Permission,util.getPermission(),"permission incorecte");
 	}
 
 	@Test
 	void testSetPseudo() {
-		fail("Not yet implemented");
+		Utilisateur util = new Utilisateur();
+		util.setPseudo(pseudo);
+		assertTrue(util.getPseudo().contains(pseudo), "pseudo incorect");
 	}
 
 }
