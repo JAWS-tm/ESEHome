7a4e9a23071004c08241b4c888f0108dc67e620e
digallle <leopold.digallo@reseau.eseo.fr>
2022-01-14 19:25:17 +0100

    Organisation temporaire du code

--- Domoteek_Server_Backend/Python/BDD/MariaDB.py
+++ Domoteek_Server_Backend/Python/BDD/MariaDB.py
@@ -1,9 +1,10 @@
 # Module Imports
-import mysql.connector
-# import mariadb
-from config.app_config import BDD_CONFIG, logger
-
+from asyncio import wait_for
+from multiprocessing.connection import wait
 import sys
+import mysql.connector
+from config.app_config import BDD_CONFIG, logger
+# import mariadb
 
 def connect():
     # Connect to MariaDB Platform
@@ -18,22 +19,23 @@ def connect():
 
         )
         logger.debug("MariaDB.py : Connected to database")
+    # except mariadb.Error as e:
     except Exception as e:
         logger.error(f"MariaDB.py : Error connecting to MariaDB Platform: {e}")
         sys.exit(1)
 
-# def connect():
-#     # Connect to MariaDB Platform
-#     try:
-#         conn = mariadb.connect(
-#             user=BDD_CONFIG["user"],
-#             password=BDD_CONFIG["password"],
-#             host=BDD_CONFIG["host"],
-#             port=BDD_CONFIG["port"],
-#             database=BDD_CONFIG["BDD_name"]
+def selectData():
+    wait_for("Dev")
 
-#         )
-#         logger.debug("MariaDB.py : Connected to database")
-#     except mariadb.Error as e:
-#         logger.error(f"MariaDB.py : Error connecting to MariaDB Platform: {e}")
-#         sys.exit(1)
+def updateData():
+    wait_for("Dev")
+
+def addData():
+    wait_for("Dev")
+
+def removeData():
+    wait_for("Dev")
+
+#Pour tester les fonctions en cours de dev 
+if __name__ == "__main__":
+    connect()
