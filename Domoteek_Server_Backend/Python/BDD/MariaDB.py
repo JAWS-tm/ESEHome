df840f8df003c157c58e87165c5eee67aa452710
digallle <leopold.digallo@reseau.eseo.fr>
2022-01-14 16:05:19 +0100

    Changement lib mariadb --> mysql-connector

--- Domoteek_Server_Backend/Python/BDD/MariaDB.py
+++ Domoteek_Server_Backend/Python/BDD/MariaDB.py
@@ -1,5 +1,6 @@
 # Module Imports
-import mariadb
+import mysql.connector
+# import mariadb
 from config.app_config import BDD_CONFIG, logger
 
 import sys
@@ -7,7 +8,8 @@ import sys
 def connect():
     # Connect to MariaDB Platform
     try:
-        conn = mariadb.connect(
+        #      mariadb.connect
+        conn = mysql.connector.connect(
             user=BDD_CONFIG["user"],
             password=BDD_CONFIG["password"],
             host=BDD_CONFIG["host"],
@@ -16,6 +18,22 @@ def connect():
 
         )
         logger.debug("MariaDB.py : Connected to database")
-    except mariadb.Error as e:
+    except mysql.Error as e:
         logger.error(f"MariaDB.py : Error connecting to MariaDB Platform: {e}")
         sys.exit(1)
+
+# def connect():
+#     # Connect to MariaDB Platform
+#     try:
+#         conn = mariadb.connect(
+#             user=BDD_CONFIG["user"],
+#             password=BDD_CONFIG["password"],
+#             host=BDD_CONFIG["host"],
+#             port=BDD_CONFIG["port"],
+#             database=BDD_CONFIG["BDD_name"]
+
+#         )
+#         logger.debug("MariaDB.py : Connected to database")
+#     except mariadb.Error as e:
+#         logger.error(f"MariaDB.py : Error connecting to MariaDB Platform: {e}")
+#         sys.exit(1)
