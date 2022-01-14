ae36909a73532927a43c3d809761c9467085c691
pcpcapi <pcpcapi@DESKTOP-K6TVDDC>
2022-01-14 17:25:02 +0100

    création DAOFactory : décommenter setParamMariaDB lorsque la BDD est mise en place

---
+++ DomESEO/src/main/java/dao/DAOFactory.java
@@ -0,0 +1,52 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DAOFactory {
+	
+	private static volatile DAOFactory instance = null;
+	private String url;
+	private String username;
+	private String password;
+	
+	private DAOFactory() {}
+	
+	public static final DAOFactory getInstance() {
+		if(DAOFactory.instance == null) {
+			synchronized(DAOFactory.class) {
+				if(DAOFactory.instance == null) {
+			        instance = new DAOFactory();
+				}
+			}
+		}
+	    return DAOFactory.instance;
+	}
+	
+	public Connection getConnection() throws SQLException {
+	    return DriverManager.getConnection(url, username, password);
+	}
+	
+	private void setParamMariaDB(String url, String username, String password) {
+	    this.url = url;
+	    this.username = username;
+	    this.password = password;		
+	    try {
+			Class.forName("org.mariadb.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} 	
+	}
+	
+	public DAOUtilisateur getDAOUtilisateur(String type) {
+		switch(type) {
+			case "MariaDB":
+				//setParamMariaDB("jdbc:mysql://", "", "");
+				return new DAOUtilisateurMariaDB(this);
+			default:
+				return null;	
+		}
+	}
+
+}
