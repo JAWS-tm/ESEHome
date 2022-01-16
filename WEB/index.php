4b077c9b860b2c8ce00f87b429bc448c5ee995bb
Raimbault PL <Raimbault PL@LAPTOP-F3S45N5N>
2022-01-16 16:53:14 +0100

    Inscription

--- WEB/index.php
+++ WEB/index.php
@@ -1,3 +1,15 @@
 <?php
-coucou
 ?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>TEST</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+ <h1>TEST</h1>
+</body>
+</html>
