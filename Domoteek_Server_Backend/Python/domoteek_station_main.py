b80c2425cc41389636c9cbdbe9e86aef72cad101
digallle <leopold.digallo@reseau.eseo.fr>
2022-01-17 16:59:34 +0100

    Création Classe BDDRequest
    Co-authored-by: QuentinMary49 <QuentinMary49@users.noreply.github.com>
    Co-authored-by: BTheo1306 <BTheo1306@users.noreply.github.com>

--- Domoteek_Server_Backend/Python/domoteek_station_main.py
+++ Domoteek_Server_Backend/Python/domoteek_station_main.py
@@ -1,11 +1,23 @@
 from config.app_config import logger
 from data_communication.FrameParser import *
-from BDD.MariaDB import *
+from BDD.BDDRequest import BDDRequest
 
 def main():
     logger.debug("-------------------Starting application-------------------")
-    connect()
+    bdd = BDDRequest()
+
+
     dummy_message = "BA1000000002EEEEEE00A2420508CAFEDECADA"#String
     #parsed_message = FrameParser()
 
 main()
+
+
+# bdd.sendData("INSERT INTO pipo (data) VALUES (%s)", (test,))
+# bdd.sendData("UPDATE pipo SET data=%s WHERE id=%s", (test,7))
+# bdd.sendData("DELETE FROM pipo WHERE id=%s", (8,))
+
+# bdd.recieveData("SELECT * FROM pipo")
+# for (id, data) in bdd.getCursor():
+#     print("ID : {}, Data : {}".format(id, data))
+
