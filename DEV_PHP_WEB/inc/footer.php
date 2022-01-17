dd3dc6d72e3838c6bb42c35bc306c2b32715bc67
Laura <laura.traczyk@free.fr>
2022-01-17 17:07:58 +0100

    footer, +head, index, styles

--- DEV_PHP_WEB/inc/footer.php
+++ DEV_PHP_WEB/inc/footer.php
@@ -3,6 +3,7 @@
 	<div class="footer_element" id="about">
 		<h2>A propos de Domoteek</h2>
 		<p>Domoteek est une société qui vous offre la possibilité de garder le contrôle sur votre domicile. Tout seul ou en famille, pilotez les différents paramètres à votre convenance pour faire de votre foyez un vrai nid douillet.<br></p>
+		<p class="grey">#domotique #IoT #FrenchTech</p>
 	</div>
 
 	<div class="footer_element" id="coordonnees">
@@ -10,7 +11,7 @@
 		<div id="box">
 			<div class="box2">
 				<i class="bi bi-pin-map"></i>
-				<p>10 Bd Jean Jeanneteau<br> 49100 Angers</p>
+				<p>10 Bd Jean Jeanneteau<br><span>49100 Angers</span></p>
 			</div>
 			<div class="box2" id="tel">
 				<i class="bi bi-telephone"></i>
@@ -18,7 +19,7 @@
 			</div>
 			<div class="box2">
 				<i class="bi bi-envelope fa-2x" ></i>
-				<p>equipe@naviguia.net</p>
+				<p>equipe@esehome.net</p>
 			</div>
 		</div>
 	</div>
@@ -26,20 +27,14 @@
 	<div class="footer_element" id="join">
 		<h2>Rejoignez-nous</h2>
 		<p>Envie de faire partie de la communauté Domoteek? Rejoignez-nous sur les différentes plateformes! Nous sommes déjà 3500 à faire partie du mouvement.<br></p>
-		<nav>
-			<ul>
-				<li><a href="http://mon-lien.com"><i class="bi bi-instagram"></i></a></li>
-				<li><a href="http://mon-lien.com"><i class="bi bi-facebook"></i></i></a></li>
-				<li><a href="http://mon-lien.com"><i class="bi bi-twitter"></i></i></a></li>
-				<li><a href="http://mon-lien.com"><i class="bi bi-github"></i></i></a></li>
-				<li><a href="http://mon-lien.com"><i class="bi bi-linkedin"></i></i></a></li>
-			</ul>
-		</nav>
+		<ul>
+			<li><a href="http://mon-lien.com"><i class="bi bi2 bi-instagram grey"></i></a></li>
+			<li><a href="http://mon-lien.com"><i class="bi bi2 bi-facebook grey"></i></i></a></li>
+			<li><a href="http://mon-lien.com"><i class="bi bi2 bi-twitter grey"></i></i></a></li>
+			<li><a href="http://mon-lien.com"><i class="bi bi2 bi-github grey"></i></i></a></li>
+			<li><a href="http://mon-lien.com"><i class="bi bi2 bi-linkedin grey"></i></i></a></li>
+		</ul>
 	</div>
-
-	<div class="footer_element" id="pictures">
-		<h2>Nos objets</h2>
-		<p>Photos<br></p>
 </footer>
  
 </body>
