fc5bedd6bb5aab0fd84829eca336a0f391fce905
Nolwenn BARMOY <nolwenn.barmoy@reseau.eseo.fr>
2022-01-17 17:29:48 +0100

    Header Nolwenn (php + css)

--- DEV_PHP_WEB/inc/header.php
+++ DEV_PHP_WEB/inc/header.php
@@ -1,7 +1,26 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
+
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>ESEhome</title>
+	<link rel="stylesheet" type="text/css" href="../css/styles.css">
+	<link rel="stylesheet" type="text/css" href="../css/header.css">
+</head>
+
+<header>
+	<div id="header">
+		<nav id="navbar" class="menu">
+			<ul>
+				<li><a href="../index.php"><img src="../img/Logo_ESEHome.PNG"></a>
+				<li><a href="../inscription.php">Inscription</a></li>
+				<li><a href="../connexion.php">Connexion</a></li>
+			</ul>
+		</nav>
+	</div>
+</header>
+
+
+<body>
 
