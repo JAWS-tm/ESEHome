a9fbe26ca50350109154a97b086ae6557a912ab4
digallle <leopold.digallo@reseau.eseo.fr>
2022-01-19 15:25:26 +0100

    Création classe dbMessage
    Co-authored-by: QuentinMary49 <QuentinMary49@users.noreply.github.com>
    Co-authored-by: BTheo1306 <BTheo1306@users.noreply.github.com>

--- Domoteek_Server_Backend/Python/domoteek_station_main.py
+++ Domoteek_Server_Backend/Python/domoteek_station_main.py
@@ -1,10 +1,10 @@
 from config.app_config import logger
 from data_communication.FrameParser import *
-from BDD.BDDRequest import BDDRequest
+from db.dbRequest import dbRequest
 
 def main():
     logger.debug("-------------------Starting application-------------------")
-    bdd = BDDRequest()
+    db = dbRequest()
 
 
     dummy_message = "BA1000000002EEEEEE00A2420508CAFEDECADA"#String
@@ -13,11 +13,6 @@ def main():
 main()
 
 
-# bdd.sendData("INSERT INTO pipo (data) VALUES (%s)", (test,))
-# bdd.sendData("UPDATE pipo SET data=%s WHERE id=%s", (test,7))
-# bdd.sendData("DELETE FROM pipo WHERE id=%s", (8,))
 
-# bdd.recieveData("SELECT * FROM pipo")
-# for (id, data) in bdd.getCursor():
-#     print("ID : {}, Data : {}".format(id, data))
+
 
