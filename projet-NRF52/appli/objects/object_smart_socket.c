eecb2d541501df1a651db5a111fc5eac99001c9b
digallle <leopold.digallo@reseau.eseo.fr>
2022-01-20 09:37:50 +0100

    Ajout de l'objet Smart Socket au projet

---
+++ projet-NRF52/appli/objects/object_smart_socket.c
@@ -0,0 +1,17 @@
+/*
+ * object_smart_socket.c
+ *
+ *  Created on: 20 janv. 2022
+ *      Author: Leopold
+ */
+
+#if OBJECT_ID == OBJECT_SMART_SOCKET
+	#include "../config.h"
+	#include "object_smart_socket.h"
+	#include "appli/common/gpio.h"
+	#include "appli/common/buttons.h"
+
+
+#endif
+
+
---
+++ projet-NRF52/appli/objects/object_smart_socket.h
@@ -0,0 +1,7 @@
+#ifndef OBJECT_SMART_SOCKETH
+#define OBJECT_SMART_SOCKETH
+#include "../config.h"
+
+void OBJECT_SMART_SOCKET_state_machine(void);
+
+#endif /* OBJECT_SMART_SOCKETH */
