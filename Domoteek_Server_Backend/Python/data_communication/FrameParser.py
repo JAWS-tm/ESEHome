e9de9ba441594e19451d108d8fbf433e8ec5fc0d
lebreuan <antoine.lebreuilly@reseau.eseo.fr>
2022-01-21 10:58:03 +0100

    creation classse FrameParser

--- Domoteek_Server_Backend/Python/data_communication/FrameParser.py
+++ Domoteek_Server_Backend/Python/data_communication/FrameParser.py
@@ -21,20 +21,6 @@ class FrameParser :
         self.end = message[-2:]
 
     def msgParsed(self):
-        # if self.msg_size != 0:
-        #     print("Trame :", self.message)
-        #     print("begin :", self.begin)
-        #     print("datasize :", self.data_size)
-        #     print("receiver :", self.receiver)
-        #     print("emmiter :", self.emmiter)
-        #     print("counter :", self.counter)
-        #     print("id :", self.id)
-        #     print("msg_size :", self.msg_size)
-        #     print("parameter color :", self.param_color)
-        #     print("data :", self.data)
-        #     print("end :", self.end)
-        # else:
-        #      print("message vide")
         CONST_BEGIN = "BA"
         CONST_END = "DA"
         self.tram_size = len(self.message)//2
@@ -53,15 +39,16 @@ class FrameParser :
         cnt = tab_msg[10] 
         id = tab_msg[11]
         msg_size = tab_msg[12]
-        data = tab_msg[13:-1]
+        param_id_e = tab_msg[13]
+        data = tab_msg[14:-1]
         end = tab_msg[-1]
         data_concat = "".join(data) 
         if begin != CONST_BEGIN:
             print("Frame error")
         elif end != CONST_END:
             print("Frame error")
-        # elif len(emitter) != 8:
-        #     print("Emitter error")
+        elif param_id_e > "24" :
+            print("Param id error")
         # elif len(reciver) != 8:
         #     print("Reciver error")
         else:
@@ -73,10 +60,12 @@ class FrameParser :
             tab_parse.insert(4,cnt)
             tab_parse.insert(5,id)
             tab_parse.insert(6,msg_size)
-            tab_parse.insert(7,data_concat)
-            tab_parse.insert(8,end)
+            tab_parse.insert(7,param_id_e)
+            tab_parse.insert(8,data_concat)
+            tab_parse.insert(9,end)
             print(tab_parse)
 
+    
 
     def getInfoMessageId(self):
         if self.id == "02":
