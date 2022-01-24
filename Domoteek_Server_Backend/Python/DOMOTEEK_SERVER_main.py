aecd1103d8ec4feae3ab37ba7fef92349d4feb9c
Florentin LEPELTIER <florentin.lepeltier@reseau.eseo.fr>
2022-01-24 09:39:06 +0100

    Uart implementation

--- Domoteek_Server_Backend/Python/DOMOTEEK_SERVER_main.py
+++ Domoteek_Server_Backend/Python/DOMOTEEK_SERVER_main.py
@@ -2,10 +2,8 @@ from config.app_config import logger
 from data_communication.FrameParser import *
 from BDD.MariaDB import *
 
-def main():
+if __name__ == "__main__":
     logger.debug("-------------------Starting application-------------------")
-    connect()
+    #connect()
     dummy_message = "BA1000000002EEEEEE00A2420508CAFEDECADA"#String
     #parsed_message = FrameParser()
-
-main()
