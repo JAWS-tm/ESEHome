88f869282abdfab1ae861e0c4ddbc7b5b123d11a
Pierre Nile <pierre.nile@reseau.eseo.fr>
2022-01-24 10:03:44 +0100

    uart communication

--- Domoteek_Server_Backend/Python/data_communication/uart_test.py
+++ Domoteek_Server_Backend/Python/data_communication/uart_test.py
@@ -1,7 +1,8 @@
 #frame = "BA1000000002EEEEEE00A2420508CAFEDECADA"
-from data_communication.UartSerial import UartSerial
+from UartSerial import UartSerial
 
-uart_reader = UartSerial("/dev/tty.usbmodem14203", 115200, 3.0)
+#uart_reader = UartSerial("/dev/tty.usbmodem14203", 115200, 3.0)
+uart_reader = UartSerial("COM3", 115200, 3.0)
 uart_reader.read_uart_frames("\n")
     
 
