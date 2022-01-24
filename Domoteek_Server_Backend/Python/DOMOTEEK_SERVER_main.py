de6244483bf86c54459cf7ced5a1a672e147fc46
Florentin LEPELTIER <florentin.lepeltier@reseau.eseo.fr>
2022-01-24 11:41:29 +0100

    Insertion in DB ok from Nucelo

--- Domoteek_Server_Backend/Python/DOMOTEEK_SERVER_main.py
+++ Domoteek_Server_Backend/Python/DOMOTEEK_SERVER_main.py
@@ -7,28 +7,36 @@ from dbProcess.dbMessage import dbMessage
 import datetime
 from config.app_config import UART_CONFIG
 from data_communication.UartController import UartController
+import traceback
 
 if __name__ == "__main__":
     logger.debug("-------------------Starting application-------------------")
+    # #TEMPORAIRE - Test de message en attendant le code et décode
+    # dummy_message = "BA1000000002EEEEEE00A2420508CAFEDECADA"#String
+    # dummy_parse = FrameParser(dummy_message)
+    # dummy_parse.msgParsed()
+    # #Ajout d'un message dans la table message
+    # message2pierre = dbMessage(db, dummy_parse.receiver, dummy_parse.emmiter ,dummy_parse.id, dummy_parse.param_color, dummy_parse.data, datetime.datetime.now() ,True)
+    # message2pierre.objectTX2DB()
+    '''MAIN'''
+    #Init values
     db = dbRequest()
-    #TEMPORAIRE - Test de message en attendant le code et décode
-    dummy_message = "BA1000000002EEEEEE00A2420508CAFEDECADA"#String
-    dummy_parse = FrameParser(dummy_message)
-    dummy_parse.msgParsed()
-    #Ajout d'un message dans la table message
-    message2pierre = dbMessage(db, dummy_parse.receiver, dummy_parse.emmiter ,dummy_parse.id, dummy_parse.param_color, dummy_parse.data, datetime.datetime.now() ,True)
-    message2pierre.objectTX2DB()
-    #Suppression d'un message de la table message
-    # messageremove = 
-    sys.exit(1)
-    # '''Sous main pour l'uart'''
-    # print("Starting UART Test")
-    # uart_controller = UartController(UART_CONFIG)
-    # while True :
-    #     incoming_msg = uart_controller.get_last_message()
-    #     if(incoming_msg!=0):
-    #         print("INCOMING MSG FROM UART : "+incoming_msg)
-
+    uart_controller = UartController(UART_CONFIG)
+    #Boucle de fond
+    while True :
+        try :
+            incoming_msg = uart_controller.get_last_message()
+            if(incoming_msg!=0):
+                print("INCOMING MSG FROM UART : "+incoming_msg)
+                parsed_message = FrameParser(incoming_msg)
+                parsed_message.msgParsed()
+                message2pierre = dbMessage(db, str(parsed_message.receiver), str(parsed_message.emitter) , str(parsed_message.id), str(parsed_message.param_id_e), str(parsed_message.data), datetime.datetime.now() ,True)
+                message2pierre.objectTX2DB()
+        except Exception as e :
+            print("ERROR : There was an error processing incoming messages")
+            print(str(e))
+            traceback.print_exc()
+            sys.exit(1)
 main()
 
 
