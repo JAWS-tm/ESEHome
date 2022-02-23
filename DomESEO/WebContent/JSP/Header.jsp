84d01cf0af932e640a9beada8ed8fab8f698bcab
Théo MALINGE--LECOMPTE <theo.malingelecompte@reseau.eseo.fr>
2022-02-23 07:43:56 +0000

    Revert "commit important CSS"
    This reverts commit 947c9a7b79fa5ac735d7ba9066afb892a9b20098

--- DomESEO/WebContent/JSP/Header.jsp
+++ DomESEO/WebContent/JSP/Header.jsp
@@ -3,7 +3,6 @@
 <html>
 <head>
 	<link rel="stylesheet" href="CSS/Header.css">
-	<link rel="stylesheet" href="CSS/Body.css">
 	<script src="LIB/jquery/jquery-3.6.0.min.js"></script>
     <script src="JS/Header.js"></script>
 </head>
@@ -14,50 +13,15 @@
 	<div id="div_droite_header">
 		<c:if test="${ connecte == 'oui' }">
 			<div id="profil_header">
-			<button id="profil_bouton"  type="button" onclick="toggle_profil_menu()">A</button>
+				<p>Faire une demande de privilège </p>
+				<button type="button" onclick="window.location.href ='Controleur?dest=add_user_page'">Demande</button>
+				<button type="button" onclick="window.location.href ='Controleur?dest=deconnexion'">Déconnexion</button>
 			</div>
 		</c:if>
 		<c:if test="${ connecte != 'oui' }">
-			<button id="connexion_bouton" type="button" onclick="ouvrir_connexion_popin()">Connexion</button>
+			<button id="connexion_bouton" class="header_bouton" type="button" onclick="ouvrir_connexion_popin()">Connexion</button>
 		</c:if>
 	</div>
 </div>
-<div id="profil_menu_popin" class="close">
-		<button type="button" class="profil_menu_bouton" onclick="window.location.href ='Controleur?dest=deconnexion'"><img class="profil_menu_bouton_icon" src="CSS/icone/editer_profil.svg"></button>
-		<button type="button" class="profil_menu_bouton" onclick="window.location.href ='Controleur?dest=deconnexion'"><img class="profil_menu_bouton_icon red_svg" src="CSS/icone/deconnexion.svg"></button>
-</div>
-<div id="connexion_popin" style="display : none;">
-	<div id="div_formulaire_connexion">
-		<div id="title">
-			<p>Connexion</p><img id="connexion_logo" src="CSS/icone/connexion.svg" />
-		</div>
-		<form method="POST" name="formulaire_connexion" action="Controleur?dest=connexion">
-			<div id="formulaire">
-				<fieldset>
-				    <legend>Pseudo</legend>
-					<input type="text" name="identifiant" />
-				</fieldset>
-			 	<fieldset>
-					<legend>Mot de passe</legend>
-					<input type="password" name="mot_de_passe" />
-				</fieldset>
-				<p style=color:red>${ error }</p>
-			</div>
-			<div id="boutton_formulaire_connexion">
-				<button id="boutton_connexion" type="submit" name="connexion">
-					<img id="check_connexion" class="green_svg" src="CSS/icone/check.svg" />
-					<p>Se connecter</p>
-				</button><br>
-				<button id="fermeture_connexion_bouton" class="" type="button" onclick="fermer_connexion_popin()">
-					<p>Fermer</p>
-					<img id="close_connexion" class="red_svg" src="CSS/icone/croix.svg" />
-				</button>
-			</div>
-		</form>
-	</div>
-</div>
-</html>
 
-<%--
-				<button type="button" onclick="window.locatioxn.href ='Controleur?dest=add_user_page'">Demande</button>
-				--%>
+</html>
--- DomESEO/src/main/java/Controleur.java
+++ DomESEO/src/main/java/Controleur.java
@@ -63,220 +63,263 @@ public class Controleur extends HttpServlet {
 		HttpSession session = request.getSession();
 		if(destination != null) {
 			
+			switch(destination) {
 			
-			
-			
-			
-			
-			
-			
-			
-			
-			if(session.getAttribute("connecte") == "oui") {
-				switch(destination) {
-				
-					case "deconnexion":
-						session.removeAttribute("connecte");
-						session.removeAttribute("id_user");
-						session.removeAttribute("admin");
-						request.getRequestDispatcher("/JSP/Accueil.jsp").forward(request, response);
-						break;		
-						
-					case "pageAdmin":
-						List<Utilisateur> liste = this.daoUtilisateur.listDB();
-						request.setAttribute("liste", liste);
-						request.getRequestDispatcher("/JSP/PageAdmin.jsp").forward(request, response);
-						break;
-						
-						
-					case "changement_groupe":
-						String groupe=request.getParameter("groups");
-						int identifiant = this.daoObjet.getIdByName(groupe);
-						this.daoUtilisateur.addgroup(identifiant);
-						request.getRequestDispatcher("/JSP/PageAdmin.jsp").forward(request, response);
-						break;
-						
-					case "modifUtil":
-						System.out.println(dest);
-						int idUtil = Integer.parseInt(request.getParameter("idUtil"));
-						Utilisateur us = daoUtilisateur.getUtilisateurById(idUtil);
-						request.setAttribute("us", us);
-						request.getRequestDispatcher("/JSP/ModifUtil.jsp").forward(request, response);
-						break;
+				case "accueil":
 					
-					case "create_user_page":
-						System.out.println(dest);
-						request.getRequestDispatcher("/JSP/CreateUser.jsp").forward(request, response);
-						break;
+					request.getRequestDispatcher("/JSP/Accueil.jsp").forward(request, response);
+					
+				break;
+					
+					
+				case "connexion":
+					String page = "/JSP/Accueil.jsp";
+					boolean error = true;
+					
+					if(request.getParameter("identifiant") != null && !request.getParameter("identifiant").isEmpty()) {
 						
-					case "create_user":
-						if((request.getParameter("pseudo") != null && !request.getParameter("pseudo").isEmpty()) && (request.getParameter("password") != null && !request.getParameter("password").isEmpty()) && (request.getParameter("confirm_password") != null && !request.getParameter("confirm_password").isEmpty())) {
-							if(request.getParameter("password").equals(request.getParameter("confirm_password"))) {
-								if(!this.daoUtilisateur.checkPseudo(request.getParameter("pseudo"))) {
-									this.daoUtilisateur.ajouterUtilisateur(request.getParameter("pseudo"), request.getParameter("password"), 0);
-									request.setAttribute("succesC", "L'utilisateur "+request.getParameter("pseudo")+" a été créé avec succès.");
-								} else { 
-									request.setAttribute("infoC", "Le pseudo a déjà été utilisé !");
-								}
-							}else {
-								request.setAttribute("infoC", "Les deux mots de passe ne sont pas équivalents !");
-							}
-						} else {
-							request.setAttribute("infoC", "Tous les champs ne sont pas complétés !");
-						}
-						request.getRequestDispatcher("/JSP/CreateUser.jsp").forward(request, response);
-						break;
+						if(request.getParameter("mot_de_passe") != null && !request.getParameter("mot_de_passe").isEmpty()) {
+					
+							if(this.daoUtilisateur.checkPseudo(request.getParameter("identifiant"))) {
 						
-					case "create_group_page":
-						System.out.println(dest);
-						request.getRequestDispatcher("/JSP/CreateGroup.jsp").forward(request, response);
-						break;
-						
-					case "create_group":
-						String groupname = request.getParameter("groupname").trim();
-						if(groupname != null && !groupname.isEmpty()) {
-							if(daoGroupe_Objet.getGroupeByNom(groupname) == null) {
-								daoGroupe_Objet.createNewGroup(groupname);
-								request.setAttribute("succes3", "Le groupe "+groupname+" a été créé.");
-							} else {
-								request.setAttribute("error", "Un groupe avec ce nom existe déjà. Veuillez réessayer.");
-							}
-						} else {
-							request.setAttribute("error", "Veuillez indiquer le nom du nouveau groupe.");
-						}
-						request.getRequestDispatcher("/JSP/CreateGroup.jsp").forward(request, response);
-						break;
-						
-					case "add_user_page":
-						request.getRequestDispatcher("/JSP/Demande_privilege.jsp").forward(request, response);
-						break;
-	
-					case "add_user":
-						if((request.getParameter("pseudo") != null && !request.getParameter("pseudo").isEmpty()) && (request.getParameter("password") != null && !request.getParameter("password").isEmpty()) && (request.getParameter("confirm_password") != null && !request.getParameter("confirm_password").isEmpty())) {
-							if(request.getParameter("password").equals(request.getParameter("confirm_password"))) {
-								if(!this.daoUtilisateur.checkPseudo(request.getParameter("pseudo"))) {
-									this.daoUtilisateur.ajouterUtilisateur(request.getParameter("pseudo"), request.getParameter("password"), -1);
-									request.setAttribute("succes", "Demande envoyée !");
-								} else { 
-									request.setAttribute("info", "Le pseudo a déjà été utilisé !");
-								}
-							}else {
-								request.setAttribute("info", "Les deux mots de passe ne sont pas équivalents !");
-							}
-						} else {
-							request.setAttribute("info", "Tous les champs ne sont pas complétés !");
-						}
-						request.getRequestDispatcher("/JSP/Demande_privilege.jsp").forward(request, response);
-						break;
-						
-					case "deleteUser":
-						int idUtil2 = Integer.parseInt(request.getParameter("idUtil2"));
-						daoUtilisateur.deleteUtilisateur(idUtil2);
-						liste = this.daoUtilisateur.listDB();
-						request.setAttribute("liste", liste);
-						request.getRequestDispatcher("/JSP/PageAdmin.jsp").forward(request, response);
-						break;						
-						
-					case "visitor_dashboard":
-						List<Objet_General> liste2 = this.daoObjet.getInfosObjets();
-						request.setAttribute("liste2", liste2);
-						request.getRequestDispatcher("/JSP/DashBoard.jsp").forward(request, response);
-						break;
-						
-					case "user_dashboard" :
-						String verif;
-						String choix = "default";
-						int idGroupe;
-						List<Objet_General> liste_default = new ArrayList<>();
-						verif = request.getParameter("verif");
-						List<Groupe> liste_groupe = this.daoGroupe_Objet.getNomGroupe(this.utilisateur.getId());
-						request.setAttribute("listeVues", liste_groupe);
-						if(verif != null) { 
-							choix = request.getParameter("choice");
-						}
-						if(choix.contains("default")) {
-							liste_default.clear();
-							liste_default = this.daoObjet.getInfosObjets();
-						}else{
-							idGroupe = Integer.valueOf(choix);
-							liste_default.clear();
-							liste_default = this.daoObjet.getInfosObjets_FromGroup(this.utilisateur.getId(), idGroupe);
-						}
-						request.setAttribute("liste2", liste_default);
-						request.getRequestDispatcher("/JSP/User_Dashboard.jsp").forward(request, response);
-						break;
-						
-					default:
-						//On ne doit pas se retrouver ici !
-						request.getRequestDispatcher("/JSP/Accueil.jsp").forward(request, response);
-						break;
-				}
-			}else {
-				switch(destination) {
-				
-					case "accueil":
-						if(session.getAttribute("connecte") == "oui") {
-							request.setAttribute("id_user", session.getAttribute("id_user"));
-							request.getRequestDispatcher("/JSP/Accueil.jsp").forward(request, response);
-						}else {
-							request.getRequestDispatcher("/JSP/Accueil.jsp").forward(request, response);
-						}
-						break;
-						
-					case "connexion":
-						String page = "/JSP/Accueil.jsp";
-						boolean error = true;
-						if(request.getParameter("identifiant") != null && !request.getParameter("identifiant").isEmpty()) {
-							if(request.getParameter("mot_de_passe") != null && !request.getParameter("mot_de_passe").isEmpty()) {
-								if(this.daoUtilisateur.checkPseudo(request.getParameter("identifiant"))) {
-									if(this.daoUtilisateur.checkLogin(request.getParameter("identifiant"), request.getParameter("mot_de_passe"))) {
-										this.utilisateur = this.daoUtilisateur.getUtilisateurByPseudo(request.getParameter("identifiant"));
-										if(this.utilisateur.getAdmin() == 1) {
-											error = false;
-											session.setAttribute("connecte", "oui");
-											session.setAttribute("id_user", this.utilisateur.getId());
-											session.setAttribute("admin", "oui");
-										} else if(this.utilisateur.getAdmin() == 0) {
-											error = false;
-											session.setAttribute("connecte", "oui");
-											session.setAttribute("id_user", this.utilisateur.getId());
-											session.setAttribute("admin", "non");
-										} else {
-											request.setAttribute("error", "Votre compte n'existe pas !");
-											request.setAttribute("privilege", "non");
-										}
+								if(this.daoUtilisateur.checkLogin(request.getParameter("identifiant"), request.getParameter("mot_de_passe"))) {
+									
+									this.utilisateur = this.daoUtilisateur.getUtilisateurByPseudo(request.getParameter("identifiant"));
+									
+									session.setAttribute("user", utilisateur);
+									
+									if(this.utilisateur.getAdmin() == 1) {
+										
+										error = false;
+										
+										request.setAttribute("connecte", "oui");
+										request.setAttribute("admin", "oui");
+										
+									} else if(this.utilisateur.getAdmin() == 0) {
+										
+										error = false;
+										request.setAttribute("connecte", "oui");
+										request.setAttribute("admin", "non");
+										
 									} else {
-										request.setAttribute("error", "Mot de passe incorrect. Veuillez réessayer.");
+										request.setAttribute("error", "Votre compte n'a pas été vérifié !");
+										request.setAttribute("privilege", "non");
 									}
+									
 								} else {
-									request.setAttribute("error", "Cet utilisateur n'existe pas.");
+									request.setAttribute("error", "Mot de passe incorrect. Veuillez réessayer.");
 								}
+							
 							} else {
-								request.setAttribute("error", "Veuillez indiquer un mot de passe.");
+								request.setAttribute("error", "Cet utilisateur n'existe pas.");
 							}
+							
 						} else {
-							request.setAttribute("error", "Veuillez indiquer le pseudonyme de l'utilisateur.");
+							request.setAttribute("error", "Veuillez indiquer un mot de passe.");
 						}
-						if(error) {
-							switch(request.getParameter("source")) {
-							case "Accueil":
-								page = "/JSP/Accueil.jsp";
-								break;
-							case "DashBoard":
-								page = "/JSP/DashBoard.jsp";
-								break;
-							default:
-								//On n'est pas censé arriver là
-								System.out.println("ERREUR : source dans connexion");
-								break;
+						
+					} else {
+						request.setAttribute("error", "Veuillez indiquer le pseudonyme de l'utilisateur.");
+					}
+					
+					
+					if(error) {
+						switch(request.getParameter("source")) {
+						case "Accueil":
+							page = "/JSP/Accueil.jsp";
+							break;
+						case "DashBoard":
+							page = "/JSP/DashBoard.jsp";
+							break;
+						default:
+							//On n'est pas censé arriver là
+							System.out.println("ERREUR : source dans connexion");
+							break;
+						}
+					}
+					request.getRequestDispatcher(page).forward(request, response);
+					break;
+				case "pageAdmin":
+					List<Utilisateur> liste = this.daoUtilisateur.listDB();
+					request.setAttribute("liste", liste);
+					request.getRequestDispatcher("/JSP/PageAdmin.jsp").forward(request, response);
+					break;	
+				case "changement_groupe":
+					String groupe=request.getParameter("groups");
+					int identifiant = this.daoObjet.getIdByName(groupe);
+					this.daoUtilisateur.addgroup(identifiant);
+					request.getRequestDispatcher("/JSP/PageAdmin.jsp").forward(request, response);
+					break;
+					
+					
+					
+				case "modifUtil":
+					
+					System.out.println(dest);
+					int idUtil = Integer.parseInt(request.getParameter("idUtil"));
+					
+					Utilisateur us = daoUtilisateur.getUtilisateurById(idUtil);
+					
+					request.setAttribute("us", us);
+					
+					request.getRequestDispatcher("/JSP/ModifUtil.jsp").forward(request, response);
+
+					
+					break;
+				
+				case "create_user_page":
+					
+					System.out.println(dest);
+					request.getRequestDispatcher("/JSP/CreateUser.jsp").forward(request, response);
+				
+					break;
+					
+				case "create_user":
+					
+					if((request.getParameter("pseudo") != null && !request.getParameter("pseudo").isEmpty()) && (request.getParameter("password") != null && !request.getParameter("password").isEmpty()) && (request.getParameter("confirm_password") != null && !request.getParameter("confirm_password").isEmpty())) {
+						
+						if(request.getParameter("password").equals(request.getParameter("confirm_password"))) {
+							
+							if(!this.daoUtilisateur.checkPseudo(request.getParameter("pseudo"))) {
+								
+								this.daoUtilisateur.ajouterUtilisateur(request.getParameter("pseudo"), request.getParameter("password"), 0);
+								request.setAttribute("succesC", "L'utilisateur "+request.getParameter("pseudo")+" a été créé avec succès.");
+								
+							} else { 
+								
+								request.setAttribute("infoC", "Le pseudo a déjà été utilisé !");
 							}
+						}else {
+							
+							request.setAttribute("infoC", "Les deux mots de passe ne sont pas équivalents !");
 						}
-						request.getRequestDispatcher(page).forward(request, response);
-						break;
-					default:
-						request.getRequestDispatcher("/JSP/Accueil.jsp").forward(request, response);
-						break;
-				}
+					} else {
+						
+						request.setAttribute("infoC", "Tous les champs ne sont pas complétés !");
+					}
+					request.getRequestDispatcher("/JSP/CreateUser.jsp").forward(request, response);
+					
+					
+					break;
+					
+					
+				case "create_group_page":
+					
+					System.out.println(dest);
+					request.getRequestDispatcher("/JSP/CreateGroup.jsp").forward(request, response);
+					
+					break;
+					
+					
+				case "create_group":
+					
+					String groupname = request.getParameter("groupname").trim();
+					
+					if(groupname != null && !groupname.isEmpty()) {
+						
+						if(daoGroupe_Objet.getGroupeByNom(groupname) == null) {
+							
+							daoGroupe_Objet.createNewGroup(groupname);
+							request.setAttribute("succes3", "Le groupe "+groupname+" a été créé.");
+
+						} else {
+							
+							request.setAttribute("error", "Un groupe avec ce nom existe déjà. Veuillez réessayer.");
+						}
+						
+						
+						
+					} else {
+						
+						request.setAttribute("error", "Veuillez indiquer le nom du nouveau groupe.");
+					}
+					
+					
+					request.getRequestDispatcher("/JSP/CreateGroup.jsp").forward(request, response);
+					
+					break;
+					
+				case "add_user_page":
+					request.getRequestDispatcher("/JSP/Demande_privilege.jsp").forward(request, response);
+						
+					break;
+
+				case "add_user":
+					
+					if((request.getParameter("pseudo") != null && !request.getParameter("pseudo").isEmpty()) && (request.getParameter("password") != null && !request.getParameter("password").isEmpty()) && (request.getParameter("confirm_password") != null && !request.getParameter("confirm_password").isEmpty())) {
+						
+						if(request.getParameter("password").equals(request.getParameter("confirm_password"))) {
+							
+							if(!this.daoUtilisateur.checkPseudo(request.getParameter("pseudo"))) {
+								
+								this.daoUtilisateur.ajouterUtilisateur(request.getParameter("pseudo"), request.getParameter("password"), -1);
+								request.setAttribute("succes", "Demande envoyée !");
+								
+							} else { 
+								
+								request.setAttribute("info", "Le pseudo a déjà été utilisé !");
+							}
+						}else {
+							
+							request.setAttribute("info", "Les deux mots de passe ne sont pas équivalents !");
+						}
+					} else {
+						
+						request.setAttribute("info", "Tous les champs ne sont pas complétés !");
+					}
+					request.getRequestDispatcher("/JSP/Demande_privilege.jsp").forward(request, response);
+					break;
+					
+					
+				case "deleteUser":
+				
+					int idUtil2 = Integer.parseInt(request.getParameter("idUtil2"));
+					
+					daoUtilisateur.deleteUtilisateur(idUtil2);
+					
+					liste = this.daoUtilisateur.listDB();
+					request.setAttribute("liste", liste);
+					
+					request.getRequestDispatcher("/JSP/PageAdmin.jsp").forward(request, response);
+
+					
+					break;
+					
+					
+				case "visitor_dashboard":
+					List<Objet_General> liste2 = this.daoObjet.getInfosObjets();
+					request.setAttribute("liste2", liste2);
+					request.getRequestDispatcher("/JSP/DashBoard.jsp").forward(request, response);
+					break;
+				case "user_dashboard" :
+					String verif;
+					String choix = "default";
+					int idGroupe;
+					List<Objet_General> liste_default = new ArrayList<>();
+					verif = request.getParameter("verif");
+					
+					List<Groupe> liste_groupe = this.daoGroupe_Objet.getNomGroupe(this.utilisateur.getId());
+					request.setAttribute("listeVues", liste_groupe);
+					
+					if(verif != null) { 
+						choix = request.getParameter("choice");
+					}
+					
+					if(choix.contains("default")) {
+						liste_default.clear();
+						liste_default = this.daoObjet.getInfosObjets();
+					}else{
+						idGroupe = Integer.valueOf(choix);
+						liste_default.clear();
+						liste_default = this.daoObjet.getInfosObjets_FromGroup(this.utilisateur.getId(), idGroupe);
+					}
+					request.setAttribute("liste2", liste_default);
+					request.getRequestDispatcher("/JSP/User_Dashboard.jsp").forward(request, response);
+					break;
+				default:
+					//On ne doit pas se retrouver ici !
+					request.getRequestDispatcher("/JSP/Accueil.jsp").forward(request, response);
+					break;
 			}
 			
 		} else {
