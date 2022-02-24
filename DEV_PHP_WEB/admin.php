578a353b785c820c6fff91a1ea91f9e0f6e563c0
Laura TRACZYK <laura.traczyk@reseau.eseo.fr>
2022-02-24 00:21:14 +0000

    messagerie user

--- DEV_PHP_WEB/admin.php
+++ DEV_PHP_WEB/admin.php
@@ -1,4 +1,9 @@
 <?php session_start();
+if (isset($_SESSION['start']) && (time() - $_SESSION['start'] > 1800)) {
+    session_unset();
+    session_destroy();
+    echo "Vous avez été inactif 30min, veuillez vous reconnecter";
+}
     require 'inc/header.php';
 ?>
     <div class="container contadm">
@@ -14,6 +19,7 @@
                     <a class="link" href="promouvoirutilisateur.php">Promouvoir un utilisateur</a><br>
                     <a class="link" href="gestionutilisateur.php">Gestion d'un utilisateur</a><br>
                     <a class="link" href="messages.php">Boite aux lettres</a><br>
+                    <a class="link" href="messages_user.php">Messagerie</a><br>
                 
                 	
     
