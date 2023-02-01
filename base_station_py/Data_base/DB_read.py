286c897d8c863128c70ad84d17879a271bbdccb1
pichouch <charles.pichou@reseau.eseo.fr>
2023-02-01 10:30:31 +0100

    restructure

--- base_station_py/Data_base/DB_read.py
+++ base_station_py/Data_base/DB_read.py
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 #all about read from database
 
-=======
-#all about read from database
-
->>>>>>> 9c9f1f97cbf1019a68a5fc30533f120b399640be
--- base_station_py/Data_comm/uart_test.py
+++ base_station_py/Data_comm/uart_test.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #frame = "BA1000000002EEEEEE00A2420508CAFEDECADA"
 from UartSerial import UartSerial
 
@@ -10,17 +9,4 @@ uart_sender = UartSerial("/dev/tty.URT0", 115200, 3.0, False, None)
 uart_sender.send_uart_frame("frame")
     
 
-=======
-#frame = "BA1000000002EEEEEE00A2420508CAFEDECADA"
-from UartSerial import UartSerial
-
-#uart_reader = UartSerial("/dev/tty.usbmodem14203", 115200, 3.0, False, None)
-#uart_reader = UartSerial("COM3", 115200, 3.0)
-#uart_reader.read_uart_frames("\n")
-
-uart_sender = UartSerial("/dev/tty.URT0", 115200, 3.0, False, None)
-uart_sender.send_uart_frame("frame")
-    
-
->>>>>>> 9c9f1f97cbf1019a68a5fc30533f120b399640be
     
