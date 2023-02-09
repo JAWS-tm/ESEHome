6a56cd3d5a690d760f1936f4a4b482c21fc6844f
Yann F <yann.fouassier@reseau.eseo.fr>
2023-02-09 16:36:03 +0100

    Ajout page nouv css

--- DEV_PHP_WEB/admin.php
+++ DEV_PHP_WEB/admin.php
@@ -10,15 +10,43 @@
 
     				<h2>Espace Administrateur de <?php echo $_SESSION['auth']->Pseudo;?></h2>
     				<p>Numero d'identifiant : <?php echo $_SESSION['auth']->id;?></p>
-    				<a class="link" href="ressource.php">Ressources</a><br>
-                    <a class="link" href="ajouterutilisateur.php">Ajouter un utilisateur</a><br>
-                    <a class="link" href="supprimerutilisateur.php">Supprimer un utilisateur</a><br>
-                    <a class="link" href="promouvoirutilisateur.php">Promouvoir un utilisateur</a><br>
-                    <a class="link" href="gestionutilisateur.php">Gestion d'un utilisateur</a><br>
-                    <a class="link" href="messages.php">Boite aux lettres</a><br>
-                    <a class="link" href="messages_user.php">Messagerie</a><br>
+                    <div class="square_button_container">
+    				    <a class="square_link" href="ressource.php">
+                            <img src="img/objects_list.png" alt="Ressources">
+                            <span>Ressources</span>
+                        </a>
+
+                        <a class="square_link" href="ajouterutilisateur.php">
+                            <img src="img/user_add.png" alt="Ajouter un utilisateur">
+                            <span>Ajouter un utilisateur</span>
+                        
+                        </a>
+                        <a class="square_link" href="supprimerutilisateur.php">
+                            <img src="img/user_remove.png" alt="Supprimer un utilisateur">
+                            <span>Supprimer un utilisateur</span>
+                        </a>
+
+                        <a class="square_link" href="promouvoirutilisateur.php">
+                            <img src="img/user_rights.png" alt="Gerer les droits">
+                            <span>Gerer les droits</span>
+                        </a>
+
+
+                        <a class="square_link" href="gestionutilisateur.php">
+                           <img src="img/manage_groups.png" alt="Gestion des groupes">
+                           <span>Gestion des groupes</span>
+                        </a>
+                        <a class="square_link" href="messages.php">
+                          <img src="img/msg_objects.png" alt="Messages objets">
+                          <span>Messages objets</span>
+                        </a>
+
+                        <a class="square_link" href="messages_user.php">
+                         <img src="img/msg_user.png" alt="Messages utilisateurs">
+                         <span>Messages utilisateurs</span>
+                        </a>
                 
-                	
+                    </div>
     
     			</div>    
     		</div>
